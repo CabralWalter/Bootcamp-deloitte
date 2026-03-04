4dd58d8baf8838d551ee6a5c953cf400df46d118
Walter Cabral <107414052+CabralWalter@users.noreply.github.com>
2026-03-04 01:29:00 -0300

    feat: add switch menu to products

--- produto/src/Main.java
+++ produto/src/Main.java
@@ -1,25 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         Product product = new Product();
 
-        System.out.print("Insira o id do produto: ");
-        product.id = sc.nextInt();
+        System.out.println("1 - Cadastrar Produto");
+        System.out.println("2 - Exibir Produto");
+        System.out.print("Escolha uma opção: ");
+        int opcao = sc.nextInt();
         sc.nextLine();
 
-        System.out.print("Insira o nome do produto: ");
-        product.name = sc.nextLine();
+        switch (opcao) {
 
-        System.out.print("Insira o preço do produto: ");
-        product.price = sc.nextDouble();
+            case 1:
+                System.out.print("Insira o id do produto: ");
+                product.id = sc.nextInt();
+                sc.nextLine();
 
-        System.out.print("Insira quantidade em estoque:");
-        product.quantity = sc.nextInt();
+                System.out.print("Insira o nome do produto: ");
+                product.name = sc.nextLine();
 
-        product.showData();
+                System.out.print("Insira o preço do produto: ");
+                product.price = sc.nextDouble();
+
+                System.out.print("Insira quantidade em estoque: ");
+                product.quantity = sc.nextInt();
+
+                break;
+
+            case 2:
+                product.showData();
+                break;
+
+            default:
+                System.out.println("Opção inválida.");
+        }
 
         sc.close();
     }
